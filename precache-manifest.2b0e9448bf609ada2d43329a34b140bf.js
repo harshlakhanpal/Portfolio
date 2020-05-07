b2d38ee9538e1ce96cfa8e3ed4a68ee568dde6b6
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-05-07 13:06:18 +0530

    Updates

---
+++ precache-manifest.2b0e9448bf609ada2d43329a34b140bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e7bc3a82400904160c165155e26ec1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b6f57a4e1619839c8148",
+    "url": "/Portfolio/static/css/main.8f8ed8c6.chunk.css"
+  },
+  {
+    "revision": "549ab3c66d58543c76a4",
+    "url": "/Portfolio/static/js/2.8d38e05d.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/Portfolio/static/js/2.8d38e05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f57a4e1619839c8148",
+    "url": "/Portfolio/static/js/main.e7a8fcf3.chunk.js"
+  },
+  {
+    "revision": "491fd985970ebf593427",
+    "url": "/Portfolio/static/js/runtime-main.b9063971.js"
+  }
+]);
